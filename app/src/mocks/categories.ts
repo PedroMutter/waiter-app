d23aa6c92e045b9c507fda91b33d6a669c1aaa41
estev3s <pedro.mutter1@gmail.com>
2022-11-18 13:01:06 -0300

    Aula 03 - Mobile PT1

---
+++ app/src/mocks/categories.ts
@@ -0,0 +1,22 @@
+export const categories = [
+  {
+    _id: '6372d7d931201a54191edcef',
+    name: 'Pizzas',
+    icon: '🍕',
+  },
+  {
+    _id: '6374301da4dc6f400c990218',
+    name: 'Bebidas',
+    icon: '🍻',
+  },
+  {
+    _id: '6376e4a2121c11baecf2c576',
+    name: 'Burgers',
+    icon: '🍔',
+  },
+  {
+    _id: '6376e4bc121c11baecf2c578',
+    name: 'Promoções',
+    icon: '🏷',
+  },
+]
---
+++ app/src/mocks/products.ts
@@ -0,0 +1,39 @@
+export const products = [
+  {
+    _id: '637424edc581fbccc12d5770',
+    name: 'Pizza quatro queijos',
+    description: 'Deliciosa pizza quatro queijos com borda simples',
+    imagePath: '1668556013703-quatro-queijos.png',
+    price: 40,
+    ingredients: [
+      {
+        name: 'Mussarela',
+        icon: '🧀',
+        _id: '637424edc581fbccc12d5771'
+      },
+      {
+        name: 'Parmesão',
+        icon: '🧀',
+        _id: '637424edc581fbccc12d5772'
+      },
+      {
+        name: 'Gouda',
+        icon: '🧀',
+        _id: '637424edc581fbccc12d5773'
+      },
+      {
+        name: 'Brie',
+        icon: '🧀',
+        _id: '637424edc581fbccc12d5774'
+      }
+    ],
+  },
+  {
+    _id: '6374303fa4dc6f400c99021e',
+    name: 'Coca cola',
+    description: 'Coca cola lata geladinha topzera',
+    imagePath: '1668558911606-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+]
